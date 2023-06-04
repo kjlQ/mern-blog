a916c0534c130194a903a98b9fac2f6934c24a9c
Roma <roma@MacBook-Air-Roma.local>
2023-06-04 22:46:23 +0300

    created update function for post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -14,7 +14,6 @@ export const create = async (req, res) => {
 
     res.json(post);
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       message: "Couldn't create article",
     });
@@ -26,7 +25,6 @@ export const getAll = async (req, res) => {
     const posts = await PostSchema.find().populate("user").exec();
     res.json(posts);
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       message: "Couldn't get articles",
     });
@@ -59,6 +57,53 @@ export const getOne = async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: "Couldn't get articles",
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostSchema.findOneAndDelete({
+      _id: postId,
+    }).then((doc) => {
+      if (!doc) {
+        return res.status(404).json({
+          message: "Can't find",
+        });
+      }
+      res.json({ success: true });
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+export const editPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    await PostSchema.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: postId,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Couldn't edit article",
     });
   }
 };
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import multer from "multer";
 
 import { registerValidaton, loginValidaton, postValidaton } from "./validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
 
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js";
+import { UserController, PostController } from "./controllers/index.js";
 
 mongoose
   .connect(dotenv.config().parsed.MANGOOSE_KEY)
@@ -16,8 +16,25 @@ mongoose
   })
   .catch((error) => console.log("DB error", error));
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 const app = express();
+
+const upload = multer({ storage });
+
 app.use(express.json());
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get("/auth/me", checkAuth, UserController.getMe);
 app.post("/auth/login", loginValidaton, UserController.login);
@@ -26,6 +43,8 @@ app.post("/auth/register", registerValidaton, UserController.register);
 app.post("/posts", checkAuth, postValidaton, PostController.create);
 app.get("/posts", checkAuth, PostController.getAll);
 app.get("/posts/:id", checkAuth, PostController.getOne);
+app.delete("/posts/:id", checkAuth, PostController.deletePost);
+app.patch("/posts/:id", checkAuth, PostController.editPost);
 
 app.listen(4444, (err) => {
   if (err) {
